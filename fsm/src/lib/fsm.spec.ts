4acd156001afef3e6f7a4d6b5f47e7598a7625e6
Igor Loskutov <igor.loskutoff@gmail.com>
2024-01-07 15:42:07 +0700

    prettier

--- fsm/src/lib/fsm.spec.ts
+++ fsm/src/lib/fsm.spec.ts
@@ -7,14 +7,13 @@ import {
   QueueItem,
   showPrintDuration0QueueItemError,
   State,
-  tick
+  tick,
 } from './fsm';
 import fc from 'fast-check';
 
-
 const BASIC_EXERCISE_PROGRAM = (() => {
   const ROUND_TYPES = ['warmup', 'exercise', 'rest', 'cooldown'] as const;
-  type RoundType = typeof ROUND_TYPES[number];
+  type RoundType = (typeof ROUND_TYPES)[number];
   type ExerciseQueueItem = QueueItem<RoundType>;
   const ONE_MINUTE = 60 * 1000;
   const TOTAL_ROUNDS = 10;
@@ -30,37 +29,54 @@ const BASIC_EXERCISE_PROGRAM = (() => {
     exercise: EXERCISE_DURATION,
     rest: REST_DURATION,
     cooldown: COOLDOWN_DURATION,
-  }
-  return [...Array(TOTAL_ROUNDS + 2/*warmup/cooldown*/).keys()].map((i): RoundType =>
-    i === 0 ? 'warmup' : i === TOTAL_ROUNDS + 1 ? 'cooldown' : i % 2 === 1 ? 'exercise' : 'rest').map((kind): ExerciseQueueItem => ({
-    kind,
-    duration: DURATIONS[kind],
-  }));
+  };
+  return [...Array(TOTAL_ROUNDS + 2 /*warmup/cooldown*/).keys()]
+    .map(
+      (i): RoundType =>
+        i === 0
+          ? 'warmup'
+          : i === TOTAL_ROUNDS + 1
+          ? 'cooldown'
+          : i % 2 === 1
+          ? 'exercise'
+          : 'rest'
+    )
+    .map(
+      (kind): ExerciseQueueItem => ({
+        kind,
+        duration: DURATIONS[kind],
+      })
+    );
 })();
-
 
 describe('fsm', () => {
   describe('push', () => {
     it('pushes', () => {
       const s0 = empty<'a'>();
-      const s1 = push([{
-        kind: 'a',
-        duration: 1,
-      }])(s0);
-      expect(s1).toEqual({
-        duration: 1,
-        queue: [{
+      const s1 = push([
+        {
           kind: 'a',
           duration: 1,
-        }],
-      } satisfies State)
+        },
+      ])(s0);
+      expect(s1).toEqual({
+        duration: 1,
+        queue: [
+          {
+            kind: 'a',
+            duration: 1,
+          },
+        ],
+      } satisfies State);
     });
     it('type is extendable', () => {
       const s0 = empty<'a' | 'b' | 'c'>();
-      const s1 = push([{
-        kind: 'a',
-        duration: 1,
-      }])(s0);
+      const s1 = push([
+        {
+          kind: 'a',
+          duration: 1,
+        },
+      ])(s0);
       type B1 = typeof s1 extends State<'a' | 'b' | 'c'> ? true : false;
       type B2 = typeof s1 extends State<'a' | 'b'> ? true : false;
       type B3 = typeof s1 extends State<'d'> ? true : false;
@@ -71,12 +87,14 @@ describe('fsm', () => {
       const _c: B3 = false;
     });
     it('no extra types leak into it', () => {
-      push([{
-        kind: 'd',
-        duration: 1,
-      // @ts-expect-error type 'd' won't be accepted here
-      }])(empty<'a' | 'b' | 'c'>());
-    })
+      push([
+        {
+          kind: 'd',
+          duration: 1,
+          // @ts-expect-error type 'd' won't be accepted here
+        },
+      ])(empty<'a' | 'b' | 'c'>());
+    });
     it('ignores <= 0 duration items, warns once', () => {
       const s0 = empty<'a'>();
       const consoleSpy = jest.spyOn(console, 'warn');
@@ -86,23 +104,27 @@ describe('fsm', () => {
       };
       const s1 = push([queueItem1])(s0);
       expect(s1).toEqual(empty());
-      expect(consoleSpy).toHaveBeenCalledWith(showPrintDuration0QueueItemError(queueItem1));
+      expect(consoleSpy).toHaveBeenCalledWith(
+        showPrintDuration0QueueItemError(queueItem1)
+      );
       const queueItem2: QueueItem<'a'> = {
         kind: 'a',
         duration: -1,
       };
       const s2 = push([queueItem2])(s0);
       expect(s2).toEqual(empty());
-      expect(consoleSpy).toHaveBeenCalledTimes(1/*warn only once*/);
-    })
+      expect(consoleSpy).toHaveBeenCalledTimes(1 /*warn only once*/);
+    });
   });
   describe('pop', () => {
     it('pops', () => {
       const s0 = empty<'a'>();
-      const s1 = push([{
-        kind: 'a',
-        duration: 1,
-      }])(s0);
+      const s1 = push([
+        {
+          kind: 'a',
+          duration: 1,
+        },
+      ])(s0);
       expect(isEmpty(s1)).toBe(false);
       const [s2] = pop(s1);
       expect(isEmpty(s2)).toBe(true);
@@ -111,104 +133,135 @@ describe('fsm', () => {
       const s0 = empty<'a'>();
       const [s1] = pop(s0);
       expect(s1).toBe(s0);
-    })
+    });
   });
   describe('tick', () => {
     it('ticks', () => {
       const s0 = empty<'a'>();
-      const s1 = push([{
-        kind: 'a',
-        duration: 2,
-      }])(s0);
+      const s1 = push([
+        {
+          kind: 'a',
+          duration: 2,
+        },
+      ])(s0);
       const [s2, queueItems] = tick(1)(s1);
       expect(s2.duration).toBe(1);
       expect(queueItems.length).toBe(0);
     });
     it('noops', () => {
       const s0 = empty<'a'>();
-      const s1 = push([{
-        kind: 'a',
-        duration: 2,
-      }])(s0);
+      const s1 = push([
+        {
+          kind: 'a',
+          duration: 2,
+        },
+      ])(s0);
       const [s2, queueItems] = tick(0)(s1);
       expect(s2).toBe(s1);
       expect(queueItems.length).toBe(0);
     });
     it('overticks', () => {
       const s0 = empty<'a'>();
-      const s1 = push([{
-        kind: 'a',
-        duration: 2,
-      }, {
-        kind: 'b',
-        duration: 3,
-      }])(s0);
+      const s1 = push([
+        {
+          kind: 'a',
+          duration: 2,
+        },
+        {
+          kind: 'b',
+          duration: 3,
+        },
+      ])(s0);
       const [s2, queueItems] = tick(4)(s1);
       expect(s2).toMatchObject({
         duration: 1,
-        queue: [{
-          kind: 'b',
-          duration: 3,
-        }],
+        queue: [
+          {
+            kind: 'b',
+            duration: 3,
+          },
+        ],
       });
-      expect(queueItems).toEqual([{
-        kind: 'a',
-        duration: 2,
-      }]);
+      expect(queueItems).toEqual([
+        {
+          kind: 'a',
+          duration: 2,
+        },
+      ]);
     });
     it('overticks too much', () => {
       const s0 = empty<'a'>();
-      const s1 = push([{
-        kind: 'a',
-        duration: 2,
-      }, {
-        kind: 'b',
-        duration: 3,
-      }])(s0);
+      const s1 = push([
+        {
+          kind: 'a',
+          duration: 2,
+        },
+        {
+          kind: 'b',
+          duration: 3,
+        },
+      ])(s0);
       const [s2] = tick(6)(s1);
       expect(s2).toEqual(empty());
     });
   });
   describe('raw simulation', () => {
     const naiveSimulationTest = (program: QueueItem<string>[]) => {
-      const [state, runLog] = program.reduce<[State<string>, QueueItem<string>[]]>(([state, queueItems], queueItem) => {
-        const [state_, queueItems_] = tick(state.duration)(state);
-        expect(queueItem).toEqual(queueItems_[0]);
-        return [state_, [...queueItems, ...queueItems_]];
-      }, [push(program)(empty()), []]);
+      const [state, runLog] = program.reduce<
+        [State<string>, QueueItem<string>[]]
+      >(
+        ([state, queueItems], queueItem) => {
+          const [state_, queueItems_] = tick(state.duration)(state);
+          expect(queueItem).toEqual(queueItems_[0]);
+          return [state_, [...queueItems, ...queueItems_]];
+        },
+        [push(program)(empty()), []]
+      );
       expect(state).toEqual(empty());
       expect(runLog).toEqual(program);
     };
-    const customTimedSimulationTest = <T extends string>(step: (currentItem: QueueItem<T>) => number) => (program: QueueItem<T>[]) => {
-      let runLog: readonly QueueItem<T>[] = [];
-      let state = push(program)(empty<T>());
-      while(!isEmpty(state)) {
-        const queueItem = currentNE(state);
-        const [state1, queueItems] = tick(step(queueItem))(state);
-        runLog = [...runLog, ...queueItems];
-        expect(runLog).toEqual(program.slice(0, runLog.length));
-        state = state1;
-      }
-    };
-    const quasirealTimerSimulationTest = customTimedSimulationTest((_currentItem) => 100);
-    const overshootTimerSimulationTest = customTimedSimulationTest((_currentItem) => 10000);
+    const customTimedSimulationTest =
+      <T extends string>(step: (currentItem: QueueItem<T>) => number) =>
+      (program: QueueItem<T>[]) => {
+        let runLog: readonly QueueItem<T>[] = [];
+        let state = push(program)(empty<T>());
+        while (!isEmpty(state)) {
+          const queueItem = currentNE(state);
+          const [state1, queueItems] = tick(step(queueItem))(state);
+          runLog = [...runLog, ...queueItems];
+          expect(runLog).toEqual(program.slice(0, runLog.length));
+          state = state1;
+        }
+      };
+    const quasirealTimerSimulationTest = customTimedSimulationTest(
+      (_currentItem) => 100
+    );
+    const overshootTimerSimulationTest = customTimedSimulationTest(
+      (_currentItem) => 10000
+    );
     it('can be used to simulate an exercise timer', () => {
       naiveSimulationTest(BASIC_EXERCISE_PROGRAM);
-    })
+    });
     it('can be used to simulate an exercise timer in real-like environment', () => {
       quasirealTimerSimulationTest(BASIC_EXERCISE_PROGRAM);
-    })
+    });
     it('can be used to simulate an exercise timer in mercury', () => {
       overshootTimerSimulationTest(BASIC_EXERCISE_PROGRAM);
-    })
-    it('passes rendomized tests', () => {
-      const randomizedExercise = fc.array(fc.record({
-        kind: fc.constantFrom(...BASIC_EXERCISE_PROGRAM.map(({ kind }) => kind)),
-        duration: fc.nat(1000 * 60 * 60 * 24).map(n => n + 1 /*no 0s*/),
-      }));
-      fc.assert(fc.property(randomizedExercise, (program) => {
-        naiveSimulationTest(program);
-      }));
     });
-  })
+    it('passes rendomized tests', () => {
+      const randomizedExercise = fc.array(
+        fc.record({
+          kind: fc.constantFrom(
+            ...BASIC_EXERCISE_PROGRAM.map(({ kind }) => kind)
+          ),
+          duration: fc.nat(1000 * 60 * 60 * 24).map((n) => n + 1 /*no 0s*/),
+        })
+      );
+      fc.assert(
+        fc.property(randomizedExercise, (program) => {
+          naiveSimulationTest(program);
+        })
+      );
+    });
+  });
 });
--- fsm/src/lib/fsm.ts
+++ fsm/src/lib/fsm.ts
@@ -6,19 +6,26 @@ const printDuration0QueueItemError = (queueItem: QueueItem<string>) => {
     duration0QueueItemErrorShown = true;
     console.warn(showPrintDuration0QueueItemError(queueItem));
   }
-}
+};
 
-export const showPrintDuration0QueueItemError = (queueItem: QueueItem<string>): string => `duration <= 0 queue item provided: ${showQueueItem(queueItem)}; this is not an error, but it is not recommended; the item will be ignored`;
+export const showPrintDuration0QueueItemError = (
+  queueItem: QueueItem<string>
+): string =>
+  `duration <= 0 queue item provided: ${showQueueItem(
+    queueItem
+  )}; this is not an error, but it is not recommended; the item will be ignored`;
 
 export type QueueItem<Kind extends string = string> = {
   kind: Kind;
   duration: number;
-}
+};
 
-const showQueueItem = <Kind extends string>(queueItem: QueueItem<Kind>): string => `${queueItem.kind}(${queueItem.duration})`;
+const showQueueItem = <Kind extends string>(
+  queueItem: QueueItem<Kind>
+): string => `${queueItem.kind}(${queueItem.duration})`;
 
 // fifo
-type Queue<Kind extends string = string> = readonly QueueItem<Kind>[]
+type Queue<Kind extends string = string> = readonly QueueItem<Kind>[];
 
 export type EmptyState = {
   duration: 0;
@@ -31,70 +38,122 @@ export type NonEmptyState<Kind extends string> = {
   // it also easier/performant to operate with micro time ticks
   duration: number;
   queue: readonly [QueueItem<Kind>, ...Queue<Kind>];
-}
+};
 
-export type State<Kind extends string = string> = NonEmptyState<Kind> | EmptyState;
+export type State<Kind extends string = string> =
+  | NonEmptyState<Kind>
+  | EmptyState;
 
 const empty_: EmptyState = {
   duration: 0,
   queue: Object.freeze([]),
 };
 
-export const empty = <Kind extends string>(): State<Kind> => empty_ as State<Kind>;
+export const empty = <Kind extends string>(): State<Kind> =>
+  empty_ as State<Kind>;
 
-export const isEmpty = <Kind extends string>(state: State<Kind>): state is EmptyState => state.queue.length === 0 && assertTrue/*defensive*/(state.duration === 0);
+export const isEmpty = <Kind extends string>(
+  state: State<Kind>
+): state is EmptyState =>
+  state.queue.length === 0 && assertTrue(/*defensive*/ state.duration === 0);
 
-export const restart = <Kind extends string>(state: State<Kind>): State<Kind> => !isRNEA(state.queue) /*nothing to restart*/ ? state : {
-  queue: state.queue,
-  duration: lastNEA(state.queue).duration,
-};
+export const restart = <Kind extends string>(state: State<Kind>): State<Kind> =>
+  !isRNEA(state.queue) /*nothing to restart*/
+    ? state
+    : {
+        queue: state.queue,
+        duration: lastNEA(state.queue).duration,
+      };
 
-export const reset = <Kind extends string>(_state: State<Kind>): State<Kind> => empty();
+export const reset = <Kind extends string>(_state: State<Kind>): State<Kind> =>
+  empty();
 
-export const push = <Kind_ extends string>(qx_: Queue<Kind_>) => <Kind extends string>(state: State<Kind_ extends Kind ? Kind : never>): State<Kind | Kind_> => {
-  const qx = Object.freeze/*just to cast to RA*/(qx_.filter((x) => {
-    if (x.duration > 0) return true;
-    printDuration0QueueItemError(x);
-    return false;
-  }).reverse());
-  if (!isRNEA(qx)) return state; // noop
-  return Object.freeze({
-    duration: state.duration && assertTrue/*defensive; duration 0 means queue is []*/(isRNEA(state.queue)) ? state.duration : lastNEA(qx).duration,
-    queue: assertRNEA(Object.freeze([...qx, ...state.queue])),
-  });
-};
+export const push =
+  <Kind_ extends string>(qx_: Queue<Kind_>) =>
+  <Kind extends string>(
+    state: State<Kind_ extends Kind ? Kind : never>
+  ): State<Kind | Kind_> => {
+    const qx = Object.freeze(
+      /*just to cast to RA*/ qx_
+        .filter((x) => {
+          if (x.duration > 0) return true;
+          printDuration0QueueItemError(x);
+          return false;
+        })
+        .reverse()
+    );
+    if (!isRNEA(qx)) return state; // noop
+    return Object.freeze({
+      duration:
+        state.duration &&
+        assertTrue(
+          /*defensive; duration 0 means queue is []*/ isRNEA(state.queue)
+        )
+          ? state.duration
+          : lastNEA(qx).duration,
+      queue: assertRNEA(Object.freeze([...qx, ...state.queue])),
+    });
+  };
 
-const popQueue = <Kind extends string>(queue: Queue<Kind>): [Queue<Kind>, QueueItem<Kind> | null] =>
-  [Object.freeze(queue.slice(0, -1)), last(queue)];
+const popQueue = <Kind extends string>(
+  queue: Queue<Kind>
+): [Queue<Kind>, QueueItem<Kind> | null] => [
+  Object.freeze(queue.slice(0, -1)),
+  last(queue),
+];
 
-export const pop = <Kind extends string>(state: State<Kind>): [State<Kind>, QueueItem<Kind> | null] => {
+export const pop = <Kind extends string>(
+  state: State<Kind>
+): [State<Kind>, QueueItem<Kind> | null] => {
   const [queue, queueItem] = popQueue(state.queue);
   if (!isRNEA(queue)) return [empty(), queueItem];
-  return [Object.freeze({
-    duration: lastNEA(queue).duration,
-    queue,
-  }), queueItem];
+  return [
+    Object.freeze({
+      duration: lastNEA(queue).duration,
+      queue,
+    }),
+    queueItem,
+  ];
 };
 
-export const currentNE = <Kind extends string>(state: NonEmptyState<Kind>): QueueItem<Kind> => lastNEA(state.queue);
+export const currentNE = <Kind extends string>(
+  state: NonEmptyState<Kind>
+): QueueItem<Kind> => lastNEA(state.queue);
 
-export const current = <Kind extends string>(state: State<Kind>): QueueItem<Kind> | null => isEmpty(state) ? null : currentNE(state);
+export const current = <Kind extends string>(
+  state: State<Kind>
+): QueueItem<Kind> | null => (isEmpty(state) ? null : currentNE(state));
 
-export const tick = (step: number/*no check, because 0s and negatives are all right, theoretically*/) => <Kind extends string>(state: State<Kind>): [State<Kind>, QueueItem<Kind>[]] => {
-  const tick_ = <Kind extends string>(step: number, state: State<Kind>, acc: QueueItem<Kind>[]): [State<Kind>, QueueItem<Kind>[]] => {
-    if (step === 0) return [state, acc]; // noop
-    if (isEmpty(state)) return [state, acc]; // noop
-    const duration = state.duration - step;
-    return (duration <= 0)/*handle overshoots*/ ? (() => {
-      const [state1, queueItem] = pop(state);
-      if (queueItem !== null) acc.push(queueItem);
-      return tick_(-duration, state1, acc)
-    })() : [Object.freeze({
-      queue: state.queue,
-      /*normal case*/
-      duration,
-    }), acc];
-  }
+export const tick =
+  (
+    step: number /*no check, because 0s and negatives are all right, theoretically*/
+  ) =>
+  <Kind extends string>(
+    state: State<Kind>
+  ): [State<Kind>, QueueItem<Kind>[]] => {
+    const tick_ = <Kind extends string>(
+      step: number,
+      state: State<Kind>,
+      acc: QueueItem<Kind>[]
+    ): [State<Kind>, QueueItem<Kind>[]] => {
+      if (step === 0) return [state, acc]; // noop
+      if (isEmpty(state)) return [state, acc]; // noop
+      const duration = state.duration - step;
+      return duration <= 0 /*handle overshoots*/
+        ? (() => {
+            const [state1, queueItem] = pop(state);
+            if (queueItem !== null) acc.push(queueItem);
+            return tick_(-duration, state1, acc);
+          })()
+        : [
+            Object.freeze({
+              queue: state.queue,
+              /*normal case*/
+              duration,
+            }),
+            acc,
+          ];
+    };
 
-  return tick_(step, state, []);
-};
+    return tick_(step, state, []);
+  };
